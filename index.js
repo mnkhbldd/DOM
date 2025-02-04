cbf37e3c984954afafb20a9bb3b6039dec616223
mnkhbldd <136554405+mnkhbldd@users.noreply.github.com>
2025-02-04 21:36:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const body = document.querySelector("body");
+const container = document.createElement("div");
+const left = document.createElement("button");
+const right = document.createElement("button");
+const nums = document.createElement("div");
+
+container.className = "cont";
+left.className = "left";
+right.className = "right";
+nums.className = "nums";
+
+body.appendChild(container);
+container.appendChild(left);
+container.appendChild(right);
+container.appendChild(nums);
+
+left.style.width = "50%";
+right.style.width = "50%";
+
+left.onclick = increase;
+right.onclick = increase;
+
+function increase(value) {
+  if (value.target.className === "left") {
+    left.style.width = parseInt(left.style.width) + 1 + "%";
+    right.style.width = parseInt(right.style.width) - 1 + "%";
+    nums.innerText = left.style.width;
+  } else {
+    left.style.width = parseInt(left.style.width) - 1 + "%";
+    right.style.width = parseInt(right.style.width) + 1 + "%";
+    nums.innerText = right.style.width;
+  }
+}
